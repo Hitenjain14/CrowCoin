2e7cc2ec29c64d4df9da694f34e8081f9e50a607
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2022-12-11 01:57:57 +0530

    to Goerli web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/93c3b8f967814b6b8313ad75682b1bbb'
+    'https://goerli.infura.io/v3/'
   );
   web3 = new Web3(provider);
 }
